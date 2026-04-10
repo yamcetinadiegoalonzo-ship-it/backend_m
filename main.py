dcdc62663d1b04d3e8c9842b3836993ef64e5d3e
Diego Alonzo Yam Cetina <yamcetinadiegoalonzo@gmail.com>
2026-04-10 15:52:38 -0600

    Fix: main limpio

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+# from fastapi import FastAPI
+
+# app = FastAPI()
+
+# @app.get("/tarea-0")
+# def tarea_0():
+#     return {"respuesta": "Primer tarea realizada"}
 from fastapi import FastAPI
 
 app = FastAPI()
+
+@app.get("/")
+def inicio():
+    return {"mensaje": "API funcionando"}
 
 @app.get("/tarea-0")
 def tarea_0():
